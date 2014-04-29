1cff8f98addb380f3038e94546f3f9a4763c0ab2
hightoon <haitong.chen@gmail.com>
2014-04-29 17:56:38 +0800

    add BStree.py

---
+++ BSTree.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+
+"""
+    implementation of binary search tree
+    reference: 
+        http://cslibrary.stanford.edu/110/BinaryTrees.html
+"""
+
+# Node data structure
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def __str__(self):
+        return "Node:%s"%(str(self.data))  
+
+
+class BSTree:
+    def __init__(self, data=None):
+        self._root = data 
+
+    def get_node(self, data):
+        return self._get_node(self._root, data)
+    
+    def _get_node(self, node, data):
+        if node is None:
+            return None
+        if node.data == data:
+            return node
+        elif data < node.data:
+            return self._get_node(node.left, data)
+        else:
+            return self._get_node(node.right, data) 
+
+    def insert(self, node, data):
+        if node is None:
+            return
+        if data < node.data:
+            if node.left is None:
+                node.left = Node(data)
+            else:
+                self.insert(node.left, data)
+        else:
+            if node.right is None:
+                node.right = Node(data)
+            else:
+                self.insert(node.right, data)
+
+    def create(self, data):
+        "create tree by insert root node"
+        self._root = Node(data)
+
+    def traverse(self, node):
+        if node is None:
+            return
+        else:
+            self.traverse(node.left)
+            print node.data 
+            self.traverse(node.right)
+
+    def depth(self, start_node, end_node):
+        if start_node is None or end_node is None:
+            return 0
+        if start_node.data < end_node.data:
+            print start_node.data, end_node.data
+            return 1 + self.depth(start_node.right, end_node)
+        elif start_node.data > end_node.data:
+            print start_node.data, end_node.data
+            return 1 + self.depth(start_node.left, end_node)
+        else:
+            return 0
+    
+    def path_length(self, node1, node2):
+        "path length from node1 to node2" 
+        return self._path_len(self._root, node1, node2)
+
+    def _path_len(self, node, node1, node2):
+        if node is None or node1 is None or node2 is None:
+            return 0
+        if (node1.data < node.data and node2.data > node.data) or\
+           (node1.data > node.data and node2.data < node.data):
+            return self.depth(node, node1) + self.depth(node, node2)
+        elif node1.data < node.data and node2.data < node.data:
+            return 1 + self._path_len(node.left, node1, node2)
+        elif node1.data > node.data and node2.data > node.data:
+            return 1 + self._path_len(node.right, node1, node2)
+        elif node.data == node1.data:
+            return self.depth(node, node2)
+        elif node.data == node2.data:
+            return self.depth(node, node1)
+
+    @property
+    def root(self):
+        return self._root
+        
+
+if __name__ == '__main__':
+    bt = BSTree()
+    bt.create(7)
+    for i in xrange(5):
+        bt.insert(bt.root, i)
+    bt.traverse(bt.root) 
+    #print bt.path_length(Node(1), Node(3))
+    n = bt.get_node(3)
+    print n.data
+
+
